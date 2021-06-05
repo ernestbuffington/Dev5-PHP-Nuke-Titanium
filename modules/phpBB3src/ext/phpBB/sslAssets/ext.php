796e5ca098b0c01a6c65803cd0c3628e6f52ca99
Ernest Buffington <ernest.buffington@gmail.com>
2021-06-04 23:13:32 -0400

    Initial Commit

---
+++ modules/phpBB3src/ext/phpBB/sslAssets/ext.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*
+* @package sslAssets
+* @copyright (c) 2007 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpBB\sslAssets;
+
+class ext extends \phpbb\extension\base
+{
+}
